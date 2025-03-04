abfc29707f94e5afdd65280265d0208bbf4452c2
tapaskumarsahoouser <iot.bariflollabs@outlook.com>
2025-03-04 12:24:57 +0530

    old project last commit

--- EcommerceInventory/urls.py
+++ EcommerceInventory/urls.py
@@ -24,10 +24,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('api/auth/', include('UserServices.urls')),
